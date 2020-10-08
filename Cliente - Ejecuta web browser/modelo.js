7cc1d5941f29263f3450c3ec3c88e7d2645d0346
ImvD <ivanmvduran@gmail.com>
2020-10-08 16:59:17 +0200

    Clase7-10-20

--- Cliente - Ejecuta web browser/modelo.js
+++ Cliente - Ejecuta web browser/modelo.js
@@ -5,18 +5,24 @@ function Juego(){
         //Para evitar que se use el mismo código
         if (!this.partidas[codigo]){
             //Si la partida no está creada, la creo
-            this.partidas[codigo] = new Partida(num,owner);
+            this.partidas[codigo] = new Partida(num,owner)
         }
         //Comprobar que no está en uso
         //Crear el objeto partida con el nº y el owner
         // this.partidas[codigo]= nueva partida
     } 
-    //Obtiene un código de 6 letras
+  
 
 
-    this.unirAPartida=function(nik){
+    this.unirAPartida=function(codigo,nik){
+        this.Partida.codigo = codigo
+        this.Partida.nickOwner = nik
+        if(this.partidas[codigo]){
+            this.partidas[codigo].agregarusuario(nick)
+        }
         //Por hacer
     }
+    //Obtiene un código de 6 letras
     this.obtenerCodigo = function(){
         let cadena = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         let letras = cadena.split(''); //split separa todos los caracteres
@@ -29,23 +35,36 @@ function Juego(){
     }
     function Partida(num,owner){
         this.maximo = num;
-        this.owner = owner;
-
+        this.nickOwner = owner;
+        this.fase = Inicial()
         
-        this.usuarios=[]; //El index 0 será el owner
+        this.usuarios={}; //El index 0 será el owner
         
         //this.usuarios={} // Version array asociativo, cada usuario tiene su nick
 
         this.agregarusuario=function(nick){
+            let nickagregar = nick
+            let contador = 1
 
-            //Por hacer
-            /*
-                Comprueba si el nick es único 
-                Comprueba si es el usuario final (n)
-            */ 
+            while(this.usuarios[nickagregar]){
+                nickagregar = nick+contador
+                contador = contador +1
+            }
+            this.usuarios[nickagregar] = new Usuario(nickagregar)
         }
-
         this.agregarusuario(owner);
+    }
+    function Inicial(){
+
+    }
+    function Jugando(){
+
+    }
+    function Final() {
+        
+    }
+    function Usuario(nick){
+        
     }
     function randomInt(low,high){
         return Math.floor(math.random()*(high - low) + low)
